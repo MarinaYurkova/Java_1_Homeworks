0e647b9649731955990e4382d47c1060df366312
MarinaYurkova <kenjisatellite@gmail.com>
2021-12-18 22:32:55 +0700

    corrected and added HomeWork2.java

--- HomeWork2.java
+++ HomeWork2.java
@@ -6,28 +6,28 @@
 */
 
 class HomeWork2 {
-    public static void main (String[] args) {
-        System.out.println (checkSum10To20(3, 5));
-        System.out.println (checkPlusOrMinus (5));
-        System.out.println (checkNegative (-6));
-        printStringNTimes ("Hallelujah!!", 5);
+    public static void main(String[] args) {
+        System.out.println(checkSum10To20(3, 5));
+        System.out.println(checkPlusOrMinus (5));
+        System.out.println(checkNegative (-6));
+        printStringNTimes("Hallelujah!!", 5);
     }
 
     static boolean checkSum10To20(int a, int b) {
         return a + b >=10 && a + b <= 20;
     }
-    
+
     static String checkPlusOrMinus(int a) {
         return a >= 0? "Positive" : "Negative";
     }
-    
+
     static boolean checkNegative(int a) {
         return a < 0;
     }
-    
-    static void printStringNTimes (String a, int b) {
+
+    static void printStringNTimes(String a, int b) {
         for (int i = 1; i < b; i++) {
-            System.out.println (a);
+            System.out.println(a);
         }
     }
 }
